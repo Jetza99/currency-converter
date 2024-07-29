aee3655aea1a47bd9d684122b15ae2cc6b034a64
Omar Marnissi <omarmarnissi777@gmail.com>
2024-07-29 15:56:23 +0100

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [currency1, setCurrency1] = useState("USD");
+  const [currency2, setCurrency2] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [output, setOutput] = useState(0);
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function convertAmount() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currency1}&to=${currency2}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok) {
+            if (currency1 === currency2)
+              throw new Error("Currencies must be different!");
+
+            throw new Error("Enter amount > 0 or < 0");
+          }
+          const data = await res.json();
+
+          setOutput(data["rates"][`${currency2}`]);
+          setIsLoading(false);
+          setError("");
+          console.log(output);
+        } catch (err) {
+          setError(err.message);
+          console.log(err.message);
+        }
+      }
+      convertAmount();
+      return function () {
+        controller.abort();
+      };
+    },
+    [currency1, currency2, amount, error, output]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) =>
+          setAmount((amount) => (amount = Number(e.target.value)))
+        }
+        disabled={isLoading}
+      />
+      <select onChange={(e) => setCurrency1((curr) => (curr = e.target.value))}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <select onChange={(e) => setCurrency2((curr) => (curr = e.target.value))}>
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <p>{error ? error : output.toFixed(3)}</p>
     </div>
   );
 }
-
-export default App;
